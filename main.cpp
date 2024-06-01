2123bfd21153d04c996d4c75b505db1a37cc5a0b
Sainan <sainan@calamity.gg>
2024-06-01 06:25:53 +0200

    Repurpose into a more general Warframe API tool

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <aes.hpp>
 #include <HttpRequest.hpp>
+#include <json.hpp>
 #include <Pattern.hpp>
 #include <Process.hpp>
 #include <ProcessHandle.hpp>
@@ -35,7 +36,7 @@ static const uint8_t iv[16] = { 49, 50, 70, 71, 66, 51, 54, 45, 76, 69, 51, 45, 
 			{
 				if (++e->second == 3)
 				{
-					std::cout << " The crumbs have been gruzzled.\n";
+					std::cout << " The crumbs have been gruzzled." << std::endl;
 					return nonce;
 				}
 			}
@@ -45,7 +46,7 @@ static const uint8_t iv[16] = { 49, 50, 70, 71, 66, 51, 54, 45, 76, 69, 51, 45, 
 			}
 		}
 	}
-	std::cout << " Failed to gruzzle the crumbs.\n";
+	std::cout << " Failed to gruzzle the crumbs." << std::endl;
 	return {};
 }
 
@@ -54,19 +55,19 @@ int main()
 	auto proc = Process::get("Warframe.x64.exe");
 	if (!proc)
 	{
-		std::cout << "Process not found.\n";
+		std::cout << "Process not found." << std::endl;
 		system("pause > nul");
 		return 1;
 	}
 	auto mod = proc->open();
-	if (!mod)
+	SOUP_IF_UNLIKELY (!mod)
 	{
-		std::cout << "Failed to open process.\n";
+		std::cout << "Failed to open process." << std::endl;
 		system("pause > nul");
 		return 2;
 	}
 	auto nonce = gruzzleNonce(*mod);
-	if (nonce.empty())
+	SOUP_IF_UNLIKELY (nonce.empty())
 	{
 		system("pause > nul");
 		return 3;
@@ -74,27 +75,42 @@ int main()
 	const std::filesystem::path localappdata = _wgetenv(L"localappdata");
 	auto eelog = string::fromFile(localappdata / "Warframe" / "EE.log");
 	auto i = eelog.find("AccountId: ");
-	if (i == std::string::npos)
+	SOUP_IF_UNLIKELY (i == std::string::npos)
 	{
-		std::cout << "Failed to find AccountId.\n";
+		std::cout << "Failed to find AccountId." << std::endl;
 		system("pause > nul");
 		return 4;
 	}
 	i += 11;
 	auto accountId = eelog.substr(i, eelog.find("\r\n", i) - i);
-	std::cout << "Checking the docket...";
+	std::cout << "?accountId=" + accountId + "&nonce=" + nonce << std::endl;
+	std::cout << "Downloading inventory... ";
+	// Note: Could also use api.warframe.com
 	HttpRequest hr("mobile.warframe.com", "/api/inventory.php?accountId=" + accountId + "&nonce=" + nonce);
 	auto res = hr.execute();
+	SOUP_IF_UNLIKELY (!res)
+	{
+		std::cout << "Request failed." << std::endl;
+		system("pause > nul");
+		return 5;
+	}
 	auto inventory = std::move(res->body);
+	auto jr = json::decode(inventory);
+	SOUP_IF_UNLIKELY (!jr)
+	{
+		std::cout << "Received an invalid response." << std::endl;
+		system("pause > nul");
+		return 6;
+	}
+	string::toFile("inventory.json", jr->encodePretty());
 	aes::pkcs7Pad(inventory);
 	aes::cbcEncrypt(
 		reinterpret_cast<uint8_t*>(inventory.data()), inventory.size(),
 		key, 16,
 		iv
 	);
-	string::toFile(localappdata / "AlecaFrame" / "lastData.dat", inventory);
-	std::cout << " Swazdo-Lah.\n";
-	std::cout << "\nAll done! Restart AlecaFrame now to see your up-to-date game data.\n";
+	string::toFile("lastData.dat", inventory);
+	std::cout << "Saved to inventory.json & lastData.dat in working directory." << std::endl;
 	system("pause > nul");
 	return 0;
 }
