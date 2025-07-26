aa9acb7c90df4fd6c529524411697b735825b6fb
Sainan <63328889+Sainan@users.noreply.github.com>
2025-07-26 11:55:34 +0200

    Also grab accountId from memory

--- main.cpp
+++ main.cpp
@@ -14,35 +14,42 @@ using namespace soup;
 static const uint8_t key[16] = { 76, 69, 79, 45, 65, 76, 69, 67, 9, 69, 79, 45, 65, 76, 69, 67 };
 static const uint8_t iv[16] = { 49, 50, 70, 71, 66, 51, 54, 45, 76, 69, 51, 45, 113, 61, 57, 0 };
 
-[[nodiscard]] static std::string gruzzleNonce(const ProcessHandle& mod)
+[[nodiscard]] static std::string gruzzleAuthz(const ProcessHandle& mod)
 {
 	std::cout << "Gruzzling";
 	std::unordered_map<std::string, int> candidates{};
-	const auto pattern = Pattern("6E 6F 6E 63 65 3D"); // nonce=
+	const auto pattern = Pattern("3F 61 63 63 6F 75 6E 74 49 64 3D"); // ?accountId=
 	for (const auto& ai : mod.getAllocations())
 	{
 		if (auto res = mod.externalScan(ai.range, pattern))
 		{
-			res = res.add(6);
-			std::string nonce;
+			res = res.add(11);
+
+			char accountId[24];
+			mod.externalRead(res, accountId, 24);
+			res = res.add(24);
+
+			res = res.add(7); // &nonce=
+
+			std::string authz = "?accountId=" + std::string(accountId, 24) + "&nonce=";
 			char c;
 			do
 			{
 				c = mod.externalRead<char>(res);
 				res = res.add(1);
-			} while (string::isNumberChar(c) && (nonce.push_back(c), true));
+			} while (string::isNumberChar(c) && (authz.push_back(c), true));
 			std::cout << ".";
-			if (auto e = candidates.find(nonce); e != candidates.end())
+			if (auto e = candidates.find(authz); e != candidates.end())
 			{
 				if (++e->second == 3)
 				{
 					std::cout << " The crumbs have been gruzzled." << std::endl;
-					return nonce;
+					return authz;
 				}
 			}
 			else
 			{
-				candidates.emplace(nonce, 1);
+				candidates.emplace(authz, 1);
 			}
 		}
 	}
@@ -66,27 +73,16 @@ int main()
 		system("pause > nul");
 		return 2;
 	}
-	auto nonce = gruzzleNonce(*mod);
-	SOUP_IF_UNLIKELY (nonce.empty())
+	auto authz = gruzzleAuthz(*mod);
+	SOUP_IF_UNLIKELY (authz.empty())
 	{
 		system("pause > nul");
 		return 3;
 	}
-	const std::filesystem::path localappdata = _wgetenv(L"localappdata");
-	auto eelog = string::fromFile(localappdata / "Warframe" / "EE.log");
-	auto i = eelog.find("AccountId: ");
-	SOUP_IF_UNLIKELY (i == std::string::npos)
-	{
-		std::cout << "Failed to find AccountId." << std::endl;
-		system("pause > nul");
-		return 4;
-	}
-	i += 11;
-	auto accountId = eelog.substr(i, eelog.find("\r\n", i) - i);
-	std::cout << "?accountId=" + accountId + "&nonce=" + nonce << std::endl;
+	std::cout << authz << std::endl;
 	std::cout << "Downloading inventory... ";
 	// Note: Could also use api.warframe.com
-	HttpRequest hr("mobile.warframe.com", "/api/inventory.php?accountId=" + accountId + "&nonce=" + nonce);
+	HttpRequest hr("mobile.warframe.com", "/api/inventory.php" + authz);
 	auto res = hr.execute();
 	SOUP_IF_UNLIKELY (!res)
 	{
